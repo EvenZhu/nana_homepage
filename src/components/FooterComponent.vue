fe326d783721a65f5ef5cc9821d8c673493b5183
祝彭辉 <even.199010@gmail.com>
2022-08-08 15:35:45 +0800

    增加全局样式； 开发eader

---
+++ src/components/FooterComponent.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="FooterComponent">
+        <pre v-text="$attrs"/>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'Footercomponent'
+    };
+</script>
---
+++ src/components/HeaderComponent.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="hello">
+    <img class="logo" src="../assets/logo.png" alt="logo">
+    <ul>
+      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">Home</a></li>
+      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">Showcase</a></li>
+      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">About</a></li>
+      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">Other</a></li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HeaderView',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.hello {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 50px;
+}
+
+.logo {
+  height: 24px;
+  width: 24px;
+}
+
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #ffffff;
+  text-decoration: none;
+}
+</style>
